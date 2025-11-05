6ee7d3f430af7690b2eccb51a733108ad2e7ff4b
Raj <rajkundur58@gmail.com>
2025-11-05 11:46:03 -0800

    Fix bugs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode: "class", // important for our toggle
+  darkMode: "class",
   theme: { extend: {} },
   plugins: [],
 };
